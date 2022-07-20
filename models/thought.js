8730a059c02550c6e46ae2468f486dd8bbc47692
Safwan Islam <saislam@davidson.edu>
2022-07-19 19:37:52 -0500

    fixed errors, running on port, time to test in insomnia

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,15 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
+const reactionSchema = new Schema({
+    reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+    reactionBody: { type: String, required: true, maxlength: 280 },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY') }
+},
+    {
+        toJSON: { getters: true },
+    }
+);
 
 const thoughtSchema = new Schema({
     thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
@@ -10,16 +20,6 @@ const thoughtSchema = new Schema({
     {
         toJSON: { virtuals: true, getters: true },
         id: false
-    }
-);
-const reactionSchema = new Schema({
-    reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
-    reactionBody: { type: String, required: true, maxlength: 280 },
-    username: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY') }
-},
-    {
-        toJSON: { getters: true },
     }
 );
 
